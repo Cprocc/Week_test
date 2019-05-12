d21f4d9b917fd1237abefd302a2036802cf38303
Cprocc <755516806@qq.com>
2019-05-12 15:45:36 +0800

    2

--- 2019-05-12柠檬微趣/2.py
+++ 2019-05-12柠檬微趣/2.py
@@ -4,6 +4,10 @@
 n = 10
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# 声明一个dic{}
+# 里面存储的格式为{int a:(int b, int c)}
+# 表示元素a在 出现的第一个位置下表是b， 最后一个位置下标是c
+# b 只在第一次访问到时更新
 dic = {}
 for i in range(n):
     if nums[i] not in dic.keys():
@@ -12,6 +16,7 @@ for i in range(n):
         temp = dic[nums[i]][0]
         dic[nums[i]] = (temp, i)
 
+# 遍历dic对每一组前后位置做减法，记录最大的
 res = 0
 for k in dic:
     res = max(dic[k][1] - dic[k][0], res)
--- 2019-05-12柠檬微趣/3.py
+++ 2019-05-12柠檬微趣/3.py
@@ -1,16 +1,26 @@
 # s = input().strip()
 
-s = 'XWAAAYZ'
+# s = 'XWAAAYZ'
+s = 'XAX'
 
+# 声明一个空的字典
 dic = {}
+# 声明一个标志变量
 flag = 1
+
+# 遍历整个字符串
 for i in range(len(s)):
+    # 如果当前字符不在字典的key中,那么添加{s[i]:i}
     if s[i] not in dic:
         dic[s[i]] = i
+
     else:
+        # 如果当前字符在字典的key中，
+        # 判断当前的字符位置i是否和字典中存的位置dic[s[i]]相邻
+        #     如果相邻字更新一下dic[s[i]]
+        #     如果不相邻则不符合题意，将标志变量更改为0
         if dic[s[i]] == i-1:
             dic[s[i]] = i
-            continue
         else:
             flag = 0
             break
