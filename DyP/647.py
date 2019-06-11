21247e0cde36c4d0612d4012959241c58d2f745c
Cprocc <755516806@qq.com>
2019-06-11 20:24:34 +0800

    6.11byebye

---
+++ DyP/647.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+
+
+if __name__ == '__main__':
+    Sol = Solution()
+    print(Sol.countSubstrings('abc'))
